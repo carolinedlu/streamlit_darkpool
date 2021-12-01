28a98e4047ba1d098bee338d47225887a496df83
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-01 15:16:06 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -61,7 +61,7 @@ with st.beta_expander("What is this app?", expanded=False):
     st.write(readme["app"]["app_intro"])
     st.write("")
 st.write("")
-st.sidebar.image(load_image("logo.png"), use_column_width=True)
+st.sidebar.image(load_image("darkpool.png"), use_column_width=True)
 display_links(readme["links"]["repo"], readme["links"]["article"])
 
 
