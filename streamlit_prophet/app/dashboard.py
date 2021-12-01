8b0285e93d1b2355fff49d326a46551ff6fe8521
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-01 17:36:42 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -74,8 +74,9 @@ with st.sidebar.beta_expander("Data", expanded=True):
 
 # Column names - change to target variable
 with st.sidebar.beta_expander("Columns", expanded=True):
- if dataset=='Credit Card Fraud':   column = st.selectbox('Select your target outcome variable',('ISFRAUD','ISFLAGGEDFRAUD'))
-    else column = st.selectbox('Select your target outcome variable',('ISFRAUD'))
+ if dataset == 'Credit Card Fraud':   
+    column = st.selectbox('Select your target outcome variable',('ISFRAUD','ISFLAGGEDFRAUD'))
+        else column = st.selectbox('Select your target outcome variable',('ISFRAUD'))
 
 
 # Launch analysis
